29fe0079213f1840e4f507df2548087d09d2d954
Andrew Logvinchuk <logan.zhi@gmail.com>
2017-11-20 21:23:46 +0200

    #8 - Add helloworld controller and test

---
+++ backend/src/test/java/io/dudes/friendsnearby/GreetingControllerTest.java
@@ -0,0 +1,28 @@
+package io.dudes.friendsnearby;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class GreetingControllerTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void shouldGreetWithHelloWorld() {
+        assertThat(restTemplate.getForObject("http://localhost/" +
+                port + "/hello", String.class)).isEqualTo("Hello world!");
+    }
+}
