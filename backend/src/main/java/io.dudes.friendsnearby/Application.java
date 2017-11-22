6324e8e3ee920835b3dbf35be7f72ed704a7bb78
Andrew Logvinchuk <logan.zhi@gmail.com>
2017-11-22 23:14:28 +0200

    #0 - Add git version tag Gradle task and endpoint

--- backend/src/main/java/io.dudes.friendsnearby/Application.java
+++ backend/src/main/java/io.dudes.friendsnearby/Application.java
@@ -1,9 +1,15 @@
 package io.dudes.friendsnearby;
 
+import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 @RestController
 @SpringBootApplication
@@ -15,5 +21,12 @@ public class Application {
     @GetMapping("/hello")
     public String greeting() {
         return "Hello world!";
+    }
+
+    @GetMapping("/version")
+    @SneakyThrows
+    public String version() {
+        List<String> strings = Files.readAllLines(Paths.get("../.git_version_tag"));
+        return strings.get(0);
     }
 }
