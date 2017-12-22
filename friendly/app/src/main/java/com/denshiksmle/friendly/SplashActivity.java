a9727182eb9f98ec9d10b7b8167f8eb934b211ab
Dmitriy Smishniy <troll3758@gmail.com>
2017-12-22 17:49:43 +0200

    start screen and app icon

---
+++ friendly/app/src/main/java/com/denshiksmle/friendly/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.denshiksmle.friendly;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        int splashTimeOut = 2000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent launchMain = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(launchMain);
+                finish();
+            }
+        }, splashTimeOut);
+    }
+}
