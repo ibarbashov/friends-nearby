68b67c3a6dab46c42f59dca54002a0c2bf4f72da
Andrew Logvinchuk <logan.zhi@gmail.com>
2017-12-24 11:51:18 +0200

    #17 - Add SpringData repositories

--- backend/src/main/java/io.dudes.friendsnearby/repository/LocationRepository.java
+++ backend/src/main/java/io.dudes.friendsnearby/repository/LocationRepository.java
@@ -1,12 +1,12 @@
 package io.dudes.friendsnearby.repository;
 
-import io.dudes.friendsnearby.entity.User;
+import io.dudes.friendsnearby.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findUserByUsername(String username);
+public interface LocationRepository extends JpaRepository<Location, Long> {
+    Optional<Location> findByUserId(Long userId);
 }
